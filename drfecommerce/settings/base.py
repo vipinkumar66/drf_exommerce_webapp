ffb301ccdf4728fd6e1963a33bb9848d5f175bf2
vipin <vipinkumarlamba55@gmail.com>
2023-06-29 10:27:50 +0530

    restframework added in settings, pytest configured

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External Packages
+    'rest_framework',
+    # Internal Apps
 ]
 
 MIDDLEWARE = [
@@ -114,3 +117,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {}
